13f153b28cf3f73fb9d007cbf098ade714a3f446
dbtmd324 <dbtmd324@gmail.com>
2023-02-19 16:53:36 +0900

    기존 파일들 업로드

---
+++ DailyMacroCrawlingBot/URLsource.py
@@ -0,0 +1,143 @@
+########## 거시경제표 URL 출처 모음 ##########
+
+#### 국내 주요지표 ####
+
+## 증시 ##
+
+# 코스피
+KOSPI = "https://finance.naver.com/sise/sise_index.nhn?code=KOSPI"
+
+# 코스닥
+KOSDAQ = "https://finance.naver.com/sise/sise_index.nhn?code=KOSDAQ"
+
+# 코스피200
+KPI200 = "https://finance.naver.com/sise/sise_index.nhn?code=KPI200"
+
+## 환율 ##
+
+# 미국 USD
+USDKRW = "https://finance.naver.com/marketindex/exchangeDetail.nhn?marketindexCd=FX_USDKRW"
+
+# 유럽연합 EUR
+EURKRW = "https://finance.naver.com/marketindex/exchangeDetail.nhn?marketindexCd=FX_EURKRW"
+
+# 일본 JPY
+JPYKRW = "https://finance.naver.com/marketindex/exchangeDetail.nhn?marketindexCd=FX_JPYKRW"
+
+# 중국 CNY
+CNYKRW = "https://finance.naver.com/marketindex/exchangeDetail.nhn?marketindexCd=FX_CNYKRW"
+
+# 영국 GBP
+GBPKRW = "https://finance.naver.com/marketindex/exchangeDetail.nhn?marketindexCd=FX_GBPKRW"
+
+# 홍콩 HKD
+HKDKRW = "https://finance.naver.com/marketindex/exchangeDetail.nhn?marketindexCd=FX_HKDKRW"
+
+# 스위스 CHF
+CHFKRW = "https://finance.naver.com/marketindex/exchangeDetail.nhn?marketindexCd=FX_CHFKRW"
+
+## 금리 ##
+
+# CD 91일
+IRR_CD91 = "https://finance.naver.com/marketindex/interestDetail.nhn?marketindexCd=IRR_CD91"
+
+# 콜금리(1일)
+IRR_CALL = "https://finance.naver.com/marketindex/interestDetail.nhn?marketindexCd=IRR_CALL"
+
+# 국고채 3년물
+IRR_GOVT_3Y = "https://finance.naver.com/marketindex/interestDetail.nhn?marketindexCd=IRR_GOVT03Y"
+
+# 회사채(AA-) 3년물
+IRR_CORP_3Y = "https://finance.naver.com/marketindex/interestDetail.nhn?marketindexCd=IRR_CORP03Y"
+
+#### 해외 주요지표 ####
+
+## US ##
+
+# DOW JONES
+DJI = "https://finance.naver.com/world/sise.nhn?symbol=DJI@DJI"
+
+# S&P 500
+SPX = "https://finance.naver.com/world/sise.naver?symbol=SPI@SPX"
+
+# NASDAQ
+IXIC = "https://finance.naver.com/world/sise.nhn?symbol=NAS@IXIC"
+
+# PHILADELPIA SEMICONDUCTOR INDEX
+PHIL = "https://finance.naver.com/world/sise.nhn?symbol=NAS@SOX&fdtc=0"
+
+## EUROPE ##
+
+# 독일 DAX 30
+DAX30 = "https://finance.naver.com/world/sise.nhn?symbol=XTR@DAX30"
+
+# 프랑스 CAC 40
+CAC40 = "https://finance.naver.com/world/sise.nhn?symbol=PAS@CAC40"
+
+# 영국 FTSE 100
+FTSE100 = "https://finance.naver.com/world/sise.nhn?symbol=LNS@FTSE100"
+
+# 이탈리아 FTSE MIB
+FTSEMIB = "https://finance.naver.com/world/sise.nhn?symbol=ITI@FTSEMIB"
+
+# 러시아 FTS
+RTSI = "https://finance.naver.com/world/sise.nhn?symbol=RUI@RTSI"
+
+## ASIA ##
+
+# 중국 상해종합
+SHS = "https://finance.naver.com/world/sise.nhn?symbol=SHS@000001"
+
+# 홍콩 항셍지수
+HSI = "https://finance.naver.com/world/sise.naver?symbol=HSI@HSI"
+
+# 일본 니케이225
+NI225 = "https://finance.naver.com/world/sise.nhn?symbol=NII@NI225"
+
+# 인도 SENSEX
+BSE30 = "https://finance.naver.com/world/sise.nhn?symbol=INI@BSE30"
+
+## CURRENCY ##
+
+# 파운드/달러
+GBPUSD = "https://finance.naver.com/marketindex/worldExchangeDetail.nhn?marketindexCd=FX_GBPUSD"
+
+# 유로/달러
+EURUSD = "https://finance.naver.com/marketindex/worldExchangeDetail.nhn?marketindexCd=FX_EURUSD"
+
+# 달러/엔
+USDJPY = "https://finance.naver.com/marketindex/worldExchangeDetail.nhn?marketindexCd=FX_USDJPY"
+
+# 달러/위안
+USDCNY = "https://finance.naver.com/marketindex/worldExchangeDetail.nhn?marketindexCd=FX_USDCNY"
+
+#### 그 외 시장지표 ####
+
+## 유가 ##
+
+# WTI(서부텍사스원유)
+WTI = "https://finance.naver.com/marketindex/worldOilDetail.nhn?marketindexCd=OIL_CL&fdtc=2"
+
+# 두바이유
+OIL_DU = "https://finance.naver.com/marketindex/worldOilDetail.nhn?marketindexCd=OIL_DU&fdtc=2"
+
+## 귀금속 ##
+
+# 금
+GOLD = "https://finance.naver.com/marketindex/worldGoldDetail.nhn?marketindexCd=CMDT_GC&fdtc=2"
+
+## 주요 원자재 ##
+
+# 구리(달러/톤)
+COPPER = "https://finance.naver.com/marketindex/materialDetail.nhn?marketindexCd=CMDT_CDY"
+
+# 옥수수
+CORN = "https://finance.naver.com/marketindex/materialDetail.nhn?marketindexCd=CMDT_C"
+
+## 기타 지표 ##
+
+# 발틱해운지수
+BDI = "https://kr.investing.com/indices/baltic-dry-historical-data"
+
+# 달러인덱스
+FX_USDX = "https://finance.naver.com/marketindex/worldExchangeDetail.nhn?marketindexCd=FX_USDX"
